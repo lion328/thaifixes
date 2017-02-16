7cc3cb24fd92b6fd2c49fde03dfe9568a280c6a5
Waritnan Sookbuntherng <lion328@hotmail.co.th>
2017-02-17 00:04:08 +0700

    Overhaul FontRendererWrapper class by using reflection and inheritance

--- src/main/java/com/lion328/thaifixes/FontRendererWrapper.java
+++ src/main/java/com/lion328/thaifixes/FontRendererWrapper.java
@@ -1,115 +1,3 @@
-/*
- * Copyright (c) 2016 Waritnan Sookbuntherng
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in all
- * copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- * SOFTWARE.
- */
-
-package com.lion328.thaifixes;
-
-import net.minecraft.client.gui.FontRenderer;
-import net.minecraft.client.renderer.texture.TextureManager;
-import net.minecraft.client.settings.GameSettings;
-import net.minecraft.util.ResourceLocation;
-
-import java.io.File;
-
-public class FontRendererWrapper extends FontRenderer
-{
-
-    public static final boolean PATCHED = false;
-
-    public FontRendererWrapper(GameSettings settings, ResourceLocation asciiTex, TextureManager texMan, boolean unicode)
-    {
-        super(settings, asciiTex, texMan, unicode);
-    }
-
-    public static File getMinecraftDirectory()
-    {
-        return null;
-    }
-
-    private static void initialize()
-    {
-
-    }
-
-    public void addRenderer(IFontRenderer renderer)
-    {
-
-    }
-
-    public void loadUnicodeTexture(int tex)
-    {
-
-    }
-
-    public byte getRawUnicodeWidth(char c)
-    {
-        return 0;
-    }
-
-    public void bindTexture(String location)
-    {
-
-    }
-
-    public int getDefaultCharacterWidth(char c)
-    {
-        return 0;
-    }
-
-    public float getX()
-    {
-        return posX;
-    }
-
-    public float getY()
-    {
-        return posY;
-    }
-
-    public float renderCharAtPos(int asciiPos, char c, boolean italic)
-    {
-        return 0;
-    }
-
-    public int getCharWidth(char c)
-    {
-        return 0;
-    }
-
-    public float getCharWidthFloat(char c)
-    {
-        return 0;
-    }
-
-    public char getLastCharacterRenderered()
-    {
-        return 0;
-    }
-}
-
-// REAL SOURCE CODE
-// Unusable because can't access private fields and methods
-// Need to use access transformer or class patch
-// Use access transformer will make conflict about obfuscated names
-/*
 package com.lion328.thaifixes;
 
 import net.minecraft.client.Minecraft;
@@ -119,6 +7,10 @@ import net.minecraft.client.settings.GameSettings;
 import net.minecraft.util.ResourceLocation;
 
 import java.io.File;
+import java.lang.invoke.MethodHandle;
+import java.lang.invoke.MethodHandles;
+import java.lang.invoke.MethodType;
+import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -128,11 +20,16 @@ public class FontRendererWrapper extends FontRenderer {
 
     public static final boolean PATCHED = true;
 
+    private static final char[] asciiCharmap = "\u00c0\u00c1\u00c2\u00c8\u00ca\u00cb\u00cd\u00d3\u00d4\u00d5\u00da\u00df\u00e3\u00f5\u011f\u0130\u0131\u0152\u0153\u015e\u015f\u0174\u0175\u017e\u0207\u0000\u0000\u0000\u0000\u0000\u0000\u0000 !\"#$%&\'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~\u0000\u00c7\u00fc\u00e9\u00e2\u00e4\u00e0\u00e5\u00e7\u00ea\u00eb\u00e8\u00ef\u00ee\u00ec\u00c4\u00c5\u00c9\u00e6\u00c6\u00f4\u00f6\u00f2\u00fb\u00f9\u00ff\u00d6\u00dc\u00f8\u00a3\u00d8\u00d7\u0192\u00e1\u00ed\u00f3\u00fa\u00f1\u00d1\u00aa\u00ba\u00bf\u00ae\u00ac\u00bd\u00bc\u00a1\u00ab\u00bb\u2591\u2592\u2593\u2502\u2524\u2561\u2562\u2556\u2555\u2563\u2551\u2557\u255d\u255c\u255b\u2510\u2514\u2534\u252c\u251c\u2500\u253c\u255e\u255f\u255a\u2554\u2569\u2566\u2560\u2550\u256c\u2567\u2568\u2564\u2565\u2559\u2558\u2552\u2553\u256b\u256a\u2518\u250c\u2588\u2584\u258c\u2590\u2580\u03b1\u03b2\u0393\u03c0\u03a3\u03c3\u03bc\u03c4\u03a6\u0398\u03a9\u03b4\u221e\u2205\u2208\u2229\u2261\u00b1\u2265\u2264\u2320\u2321\u00f7\u2248\u00b0\u2219\u00b7\u221a\u207f\u00b2\u25a0\u0000".toCharArray();
+
     private static Map<String, ResourceLocation> resourceLocationPool = new HashMap<String, ResourceLocation>();
 
     private static List<IFontRenderer> renderers = new ArrayList<IFontRenderer>();
     private TextureManager renderEngine;
     private char lastChar = 0;
+
+    private static MethodHandle loadGlyphTextureMethodHandle;
+    private static MethodHandle getCharWidthFloatMethodHandle;
 
     public static File getMinecraftDirectory() {
         return Minecraft.getMinecraft().mcDataDir;
@@ -145,13 +42,31 @@ public class FontRendererWrapper extends FontRenderer {
     }
 
     public void addRenderer(IFontRenderer renderer) {
-        if (renderers.contains(renderer)) return;
+        if (renderers.contains(renderer))
+        {
+            return;
+        }
+
         renderer.setFontRendererWrapper(this);
         renderers.add(renderer);
     }
 
     public void loadUnicodeTexture(int tex) {
-        loadGlyphTexture(tex);
+        if (loadGlyphTextureMethodHandle == null)
+        {
+            ThaiFixes.LOGGER.error("Can't load unicode texture, method not found");
+
+            return;
+        }
+
+        try
+        {
+            loadGlyphTextureMethodHandle.invokeExact(this, tex);
+        }
+        catch (Throwable throwable)
+        {
+            ThaiFixes.LOGGER.catching(throwable);
+        }
     }
 
     public byte getRawUnicodeWidth(char c) {
@@ -164,10 +79,6 @@ public class FontRendererWrapper extends FontRenderer {
         renderEngine.bindTexture(resourceLocationPool.get(location));
     }
 
-    public int getDefaultCharacterWidth(char c) {
-        return super.getCharWidth(c);
-    }
-
     public float getX() {
         return posX;
     }
@@ -177,50 +88,103 @@ public class FontRendererWrapper extends FontRenderer {
     }
 
     @Override
-    public float renderCharAtPos(int asciiPos, char c, boolean italic) {
-        float ret = Float.NaN;
-        for (IFontRenderer renderer : renderers)
-            if (renderer.isSupportedCharacter(c)) {
-                ret = renderer.renderCharacter(c, italic);
-                break;
-            }
-        if (Float.isNaN(ret))
-            ret = super.renderCharAtPos(asciiPos, c, italic);
-        lastChar = c;
+    protected float renderDefaultChar(int c, boolean italic)
+    {
+        float ret = super.renderDefaultChar(c, italic);
+
+        lastChar = asciiCharmap[c];
+
         return ret;
     }
 
-    public float renderCharAtPos(char c, boolean italic) {
-        float ret = Float.NaN;
-        for (IFontRenderer renderer : renderers)
-            if (renderer.isSupportedCharacter(c)) {
-                ret = renderer.renderCharacter(c, italic);
-                break;
-            }
-        if (Float.isNaN(ret))
-            ret = super.renderCharAtPos(c, italic);
+    @Override
+    protected float renderUnicodeChar(char c, boolean italic)
+    {
+        IFontRenderer renderer = getCharacterAvailableRenderer(c);
+
+        float ret = 0.0F;
+
+        if (renderer == null)
+        {
+            super.renderUnicodeChar(c, italic);
+        }
+        else
+        {
+            ret = renderer.getCharacterWidth(c);
+        }
+
         lastChar = c;
+
         return ret;
     }
 
     @Override
     public int getCharWidth(char c) {
-        for (IFontRenderer renderer : renderers)
-            if (renderer.isSupportedCharacter(c))
-                return renderer.getCharacterWidth(c);
+        IFontRenderer renderer = getCharacterAvailableRenderer(c);
+
+        if (renderer != null)
+        {
+            return renderer.getCharacterWidth(c);
+        }
+
         return super.getCharWidth(c);
     }
 
-    public float getCharWidthFloat(char c) {
-        for (IFontRenderer renderer : renderers)
-            if (renderer.isSupportedCharacter(c))
-                return (float) renderer.getCharacterWidth(c);
-        return super.getCharWidthFloat(c);
+    public float getCharacterWidthFloat(char c) {
+        IFontRenderer renderer = getCharacterAvailableRenderer(c);
+
+        if (renderer != null)
+        {
+            return renderer.getCharacterWidth(c);
+        }
+
+        return 0;
     }
 
     public char getLastCharacterRenderered() {
         return lastChar;
     }
-}
 
- */
+    public IFontRenderer getCharacterAvailableRenderer(char c)
+    {
+        for (IFontRenderer renderer : renderers)
+        {
+            if (renderer.isSupportedCharacter(c))
+            {
+                return renderer;
+            }
+        }
+
+        return null;
+    }
+
+    static
+    {
+        MethodHandles.Lookup lookup = MethodHandles.lookup();
+
+        try
+        {
+            Method method = FontRenderer.class.getMethod("loadGlyphTexture");
+            method.setAccessible(true);
+
+            loadGlyphTextureMethodHandle = lookup.unreflect(method);
+        }
+        catch (NoSuchMethodException | IllegalAccessException e)
+        {
+            ThaiFixes.LOGGER.catching(e);
+        }
+
+        try
+        {
+            getCharWidthFloatMethodHandle = lookup.findVirtual(FontRenderer.class, "getCharWidthFloat", MethodType.methodType(float.class, char.class));
+        }
+        catch (NoSuchMethodException ignore)
+        {
+
+        }
+        catch (IllegalAccessException e)
+        {
+            ThaiFixes.LOGGER.catching(e);
+        }
+    }
+}
