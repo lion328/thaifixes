27bcf3d2758288f0c28b8e5bea4cd08d5800ea0b
Waritnan Sookbuntherng <lion328@hotmail.co.th>
2018-08-25 23:07:30 +0700

    Move from LiteLoader to Rift

--- src/main/java/com/lion328/thaifixes/ThaiFixes.java
+++ src/main/java/com/lion328/thaifixes/ThaiFixes.java
@@ -2,11 +2,19 @@ package com.lion328.thaifixes;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.dimdev.riftloader.listener.InitializationListener;
+import org.spongepowered.asm.mixin.Mixins;
 
-public class ThaiFixes
+public class ThaiFixes implements InitializationListener
 {
 
     private static Logger logger;
+
+    @Override
+    public void onInitialization()
+    {
+        Mixins.addConfiguration("mixins.thaifixes.json");
+    }
 
     public static Logger getLogger()
     {
--- src/main/java/com/lion328/thaifixes/mixin/MixinFontRenderer.java
+++ src/main/java/com/lion328/thaifixes/mixin/MixinFontRenderer.java
@@ -1,6 +1,7 @@
 package com.lion328.thaifixes.mixin;
 
 import com.lion328.thaifixes.FontRendererWrapper;
+import com.lion328.thaifixes.ThaiFixes;
 import net.minecraft.client.gui.FontRenderer;
 import org.spongepowered.asm.mixin.*;
 import org.spongepowered.asm.mixin.injection.At;
@@ -14,35 +15,9 @@ public abstract class MixinFontRenderer
 
     private char lastChar = 0;
 
-    @Shadow
-    private float posX;
-    @Shadow
-    private float posY;
-
-    @Shadow
-    protected abstract int getCharWidth(char c);
-
     @Inject(method = "<init>*", at = @At("RETURN"))
     protected void onConstructed(CallbackInfo ci)
     {
-
-    }
-
-    @Inject(method = "renderChar(CZ)F", at = @At("HEAD"))
-    protected void onRenderChar(char c, boolean italic, CallbackInfoReturnable<Float> cir)
-    {
-
-    }
-
-    @Inject(method = "getCharWidthFloat(C)F", remap = false, at = @At("HEAD"))
-    protected void onGetCharWidthFloat(char c, CallbackInfoReturnable<Float> cir)
-    {
-
-    }
-
-    @Unique
-    protected float getCharWidthFloat(char c)
-    {
-        return (float) getCharWidth(c);
+        ThaiFixes.getLogger().info("Hello Mixin!");
     }
 }
