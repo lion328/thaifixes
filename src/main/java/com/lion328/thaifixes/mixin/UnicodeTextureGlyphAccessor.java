a7c812329ef941b50f6d3ba0f1e8b71d5a894c89
Waritnan Sookbuntherng <lion328@hotmail.co.th>
2019-10-25 01:56:03 +0700

    Workaround of incomplete inner class refmap, dev env is broken
    See also: https://github.com/FabricMC/fabric-mixin-compile-extensions/issues/1

--- src/main/java/com/lion328/thaifixes/mixin/UnicodeTextureGlyphAccessor.java
+++ src/main/java/com/lion328/thaifixes/mixin/UnicodeTextureGlyphAccessor.java
@@ -27,7 +27,7 @@ import net.minecraft.client.texture.NativeImage;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.gen.Accessor;
 
-@Mixin(targets = "net.minecraft.client.font.UnicodeTextureFont$UnicodeTextureGlyph")
+@Mixin(targets = "net.minecraft.client.font.UnicodeTextureFont.UnicodeTextureGlyph")
 public interface UnicodeTextureGlyphAccessor extends RenderableGlyph
 {
     @Accessor
