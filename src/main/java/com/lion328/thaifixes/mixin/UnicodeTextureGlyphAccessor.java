b33728211df223abc0c62e4b057f6a55c5392f9f
Waritnan Sookbuntherng <lion328@hotmail.co.th>
2019-10-26 17:59:06 +0700

    Ugly working dev and prod env workaround for the missing of UnicodeTextureGlyph refmap

--- src/main/java/com/lion328/thaifixes/mixin/UnicodeTextureGlyphAccessor.java
+++ src/main/java/com/lion328/thaifixes/mixin/UnicodeTextureGlyphAccessor.java
@@ -27,7 +27,8 @@ import net.minecraft.client.texture.NativeImage;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.gen.Accessor;
 
-@Mixin(targets = "net.minecraft.client.font.UnicodeTextureFont.UnicodeTextureGlyph")
+@Mixin(targets = {"net.minecraft.client.font.UnicodeTextureFont.UnicodeTextureGlyph",
+        "net.minecraft.client.font.UnicodeTextureFont$UnicodeTextureGlyph"})
 public interface UnicodeTextureGlyphAccessor extends RenderableGlyph
 {
     @Accessor
