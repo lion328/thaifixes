7d6d93ec23ba841bf426f9b8f36c5b6c4debe750
Waritnan Sookbuntherng <lion328@hotmail.co.th>
2022-02-10 16:03:56 +0700

    Generated ModInformation

--- src/template/java/com/lion328/thaifixes/ModInformation.java
+++ src/template/java/com/lion328/thaifixes/ModInformation.java
@@ -25,8 +25,8 @@ package com.lion328.thaifixes;
 public class ModInformation
 {
 
-    public static final String VERSION = "%VERSION%";
-    public static final String MCVERSION = "%MCVERSION%";
-    public static final String MODID = "thaifixes";
-    public static final String NAME = "ThaiFixes";
+    public static final String VERSION = "${version}";
+    public static final String MCVERSION = "${mcversion}";
+    public static final String MODID = "${modid}";
+    public static final String NAME = "${name}";
 }
